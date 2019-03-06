bb3d0218031e639d8ab82898103dd1a172c48955
Leon <leon@wp2static.com>
2019-03-06 19:41:03 +0700

    add deploy options to JS

--- admin/js/wp2static-addon-azure-admin.js
+++ admin/js/wp2static-addon-azure-admin.js
@@ -27,6 +27,25 @@
 	 * single DOM-ready or window-load handler for a particular page.
 	 * Although scripts in the WordPress core, Plugins and Themes may be
 	 * practising this, we should strive to set a better example in our own work.
-	 */
+   */
+
+	$(function() {
+    deploy_options['azure'] = {
+      exportSteps: [
+          'azure_prepare_export',
+          'azure_upload_files',
+          'finalize_deployment'
+      ],
+      required_fields: {
+        azStorageAccountName: 'Please specify your Storage Account Name in order to deploy to Azure.',
+        azContainerName: 'Please specify your Container Name in order to deploy to Azure.',
+        azAccessKey: 'Please specify your Access Key for this Storage/Container.'
+      }
+    };
+
+    status_descriptions['azure_prepare_export'] = 'Preparing to deploy to Microsoft Azure Storage';
+    status_descriptions['azure_upload_files'] = 'Uploading files to Microsoft Azure Storage';
+
+  }); // end DOM ready
 
 })( jQuery );
