e67a27834adfdc263d715425d21869797d3c0689
Leon <leon@wp2static.com>
2019-04-15 19:21:41 +0700

    rm cruft

--- admin/class-wp2static-addon-azure-admin.php
+++ admin/class-wp2static-addon-azure-admin.php
@@ -1,80 +1,17 @@
 <?php
 
-/**
- * The admin-specific functionality of the plugin.
- *
- * @link       https://leonstafford.github.io
- * @since      1.0.0
- *
- * @package    Wp2static_Addon_Azure
- * @subpackage Wp2static_Addon_Azure/admin
- */
-
-/**
- * The admin-specific functionality of the plugin.
- *
- * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the admin-specific stylesheet and JavaScript.
- *
- * @package    Wp2static_Addon_Azure
- * @subpackage Wp2static_Addon_Azure/admin
- * @author     Leon Stafford <leon@wp2static.com>
- */
 class Wp2static_Addon_Azure_Admin {
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
 	private $plugin_name;
-
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
 	private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
 	public function __construct( $plugin_name, $version ) {
-
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
 
 	}
 
-	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wp2static_Addon_Azure_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wp2static_Addon_Azure_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp2static-addon-azure-admin.js', array( 'jquery' ), $this->version, false );
-
 	}
-
 }
